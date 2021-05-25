5591413ac33f691eeaee180d231bbaaf7ed44cad
Osheit <chang.osheit@gmail.com>
2021-05-25 19:52:26 -0400

    navbar

---
+++ src/components/Header.js
@@ -0,0 +1,103 @@
+import React from "react";
+import styled from "styled-components";
+
+function Header(props) {
+  return (
+    <div className="header">
+      <Nav>
+        <Logo>
+          <img src="/images/logo.svg" alt="Disney+" />
+        </Logo>
+        <NavMenu>
+        <a href="/home">
+            <img src="./images/home-icon.svg" alt="home" />
+        <span>Home</span>
+        </a>
+        </NavMenu>
+
+      </Nav>
+    </div>
+  );
+}
+
+const Nav = styled.nav`
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  height: 70px;
+  background-color: #090b13;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 36px;
+  letter-spacing: 16px;
+  z-index: 3;
+`;
+
+const Logo = styled.a`
+  padding: 0;
+  width: 80px;
+  margin-top: 4px;
+  max-height: 70px;
+  font-size: 0;
+  display: inline-block;
+
+  img {
+    display: block;
+    width: 100%;
+  }
+`;
+
+const NavMenu = styled.div`
+    align-items: center;
+    display: flex;
+    flex-flow: row nowrap;
+    height: 100%;
+    justify-content: flex-end;
+    margin: 0px;
+    padding: 0px;
+    position: relative;
+    margin-right: auto;
+    margin-left: 25px;
+
+  a {
+      display: flex;
+      align-center: center;
+      padding: 0 12px;
+
+    img {
+        height: 20px;
+        min-width: 20px;
+        width: 20px;
+        
+    }
+
+    span {
+        color: rgb(249, 249, 249);
+        font-size: 13px;
+        letter-spacing: 1.42px;
+        line-height: 1.08;
+        padding: 2px 0px;
+        white-space: nowrap;
+        position: relative;
+    }
+
+    &:before {
+      display: block;
+      background-color: rgb(249, 249, 249);
+      border-radius: 0px 0px 4px 4px;
+      bottom: -6px;
+      
+    }
+
+  }
+
+/* @media (max-width: 786px){
+        display: none;
+    }
+`;  
+
+
+
+export default Header;
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,22 @@
-import React from "styled-components";
+import React from "react";
 import styled from "styled-components";
 
 function Login() {
   return (
     <div className="login">
       <Container>
-        <Content>content</Content>
+        <Content>
+          <CTA>
+            <CTALogoOne src="/images/cta-logo-one.svg" alt="" />
+            <SignUp>GET ALL THERE</SignUp>
+            <Description> Get Premier Access to Rayaand the Last Dragon an additional fee with a Disney+
+            Subscription. As of 07/12/21, the price of Disney+ 
+            and the Disney Bundle will increase by $1
+            </Description>
+            <CTALogoTwo src="/images/cta-logo-two.png" alt="" />
+          </CTA>
+          <BgImage />
+        </Content>
       </Container>
     </div>
   );
@@ -19,18 +30,85 @@ const Container = styled.section`
   height: 100vh;
 `;
 
+const BgImage = styled.div`
+  background-image: url("/images/login-background.jpg");
+  height: 100%;
+  background-position: top;
+  background-size: cover;
+  background-repeat: no-repeat;
+  position: absolute;
+  top: 0;
+  right: 0;
+  left: 0;
+  z-index: -1;
+`;
+
 const Content = styled.div`
-    margin-bottom: 10vw;
+  margin-bottom: 10vw;
+  width: 100%;
+  position: relative;
+  min-height: 100vh;
+  box-sizing: border-box;
+  display: flex;
+  justify-content: center;
+  align-item: center;
+  flex-direction: column;
+  padding: 80px 40px;
+  height: 100%;
+`;
+
+const CTA = styled.div`
+  margin-bottom: 2vw;
+  max-width: wrap;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  margin-top: 0;
+  align-items: center;
+  text-align: center;
+  margin-right: auto;
+  margin-left: auto;
+`;
+
+const CTALogoOne = styled.img`
+  margin-bottom: 12px;
+  max-width: 600px;
+  min-height: 1px;
+  display: block;
+  width: 100%;
+`;
+
+const SignUp = styled.a`
+    font-weight: bold;
+    color: #f9f9f9;
+    background-color: #0063e5;
+    margin-bottom: 12px;
     width: 100%;
-    position: relative;
-    min-height: 100vh;
-    box-sizing: border-box;
-    display: flex;
-    justify-content: center;
-    align-item:center;
-    flex-direction: column;
-    padding: 80px 40px;
-    height: 100%;
+    letter-spacing: 1.5px;
+    font-size: 18px;
+    padding: 16.5px 0;
+    border: 1px solid transparent;
+    border-radius: 4px;
+    &:hover {
+        background-color: #0483ee;
+    }
+`;
+
+const Description = styled.p`
+    color: hsla(0,0%, 95.3%, 1);
+    font-size: 11px;
+    margin: 0 0 24px;
+    line-height: 1.5em;
+    letter-spacing: 1.5px;
+`;
+
+const CTALogoTwo = styled.img`
+    max-width: 600px;
+    margin-bottom: 20px;
+    display: inline-block;
+    vertical-align: bottom;
+    width: 100%;
+    background-image: url("image/cta-logo-two.png")
 `;
 
 export default Login;
